b9fd043ff1105cb53bb0d964846987348aff1d5e
Maria Pizzarro <maria.pizzarro@plastic-center.com>
2021-02-10 14:48:16 -0300

    Interruptor

---
+++ night.js
@@ -0,0 +1,7 @@
+const nightMode = document.querySelector('#switch-shadow')
+
+nightMode.addEventListener('click', () => {
+    
+    document.documentElement.classList.toggle('switch-shadow')
+  })
+
